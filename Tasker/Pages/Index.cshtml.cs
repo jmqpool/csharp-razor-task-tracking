9576ecae8c961ed8011f4a4f731c31afb7a36b61
Jamie Pool <japoo@microsoft.com>
2021-03-12 12:11:36 -0800

    Section 5 Updates

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -9,8 +9,20 @@ namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+
+        public IEnumerable<Task> Tasks;
+
+
+        private ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context) {
+            context = _context;
+        }
+
         public void OnGet()
         {
+            Tasks = _context.Tasks;
+            Tasks.OrderBy(x => x.Priority);
+                
         }
     }
 }
